5155364952970c318aec4dbc0fd614b79349eda6
Ansab Naif <ansab20556@gmail.com>
2025-08-13 23:40:56 +0300

    Add LoggerTrait for logging actions

---
+++ src/LoggerTrait.php
@@ -0,0 +1,10 @@
+<?php
+namespace LibrarySystem;
+
+trait LoggerTrait
+{
+    public function log(string $message): void
+    {
+        echo "[LOG] " . date("Y-m-d H:i:s") . " - {$message}<br>";
+    }
+}
